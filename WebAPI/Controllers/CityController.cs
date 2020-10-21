f18a045daa50efe4dc368a5e5ba1e747e70ab509
ashiq97 <ashiq070@gmail.com>
2020-10-21 15:14:45 +0600

    added my project

---
+++ WebAPI/Controllers/CityController.cs
@@ -0,0 +1,59 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Interfaces;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CityController : ControllerBase
+    {
+        private readonly IUnitOfWork uow;
+
+        public CityController(IUnitOfWork uow)
+        {
+            this.uow = uow;
+        }
+
+        // GET api/city
+        [HttpGet]
+        public async Task<IActionResult> GetCities()
+        {
+            var cities = await uow.CityRepository.GetCitiesAsync();
+            return Ok(cities);
+        }
+
+        // Post api/city/post --Post the data in JSON Format
+        [HttpPost("post")]
+        public async Task<IActionResult> AddCity(City city)
+        {
+            uow.CityRepository.AddCity(city);
+            await uow.SaveAsync();
+            return StatusCode(201);
+        }
+
+
+        [HttpDelete("delete/{id}")]
+        public async Task<IActionResult> DeleteCity(int id)
+        {
+            uow.CityRepository.DeleteCity(id);
+            await uow.SaveAsync();
+            return Ok(id);
+        }
+
+        // Post api/city/add?cityname=Miami
+        // Post api/city/add/Los Angeles
+        // [HttpPost("add")]
+        // [HttpPost("add/{cityname}")]
+        // public async Task<IActionResult> AddCity(string cityName)
+        // {
+        //     City city = new City();
+        //     city.Name = cityName;
+        //     await dc.Cities.AddAsync(city);
+        //     await dc.SaveChangesAsync();
+        //     return Ok(city);
+        // }
+    }
+}
+
---
+++ WebAPI/Startup.cs
@@ -0,0 +1,52 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using WebAPI.Data;
+using WebAPI.Interfaces;
+
+namespace WebAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {            
+            services.AddDbContext<DataContext>(options => 
+            options.UseSqlServer(Configuration.GetConnectionString("Default")));
+            services.AddControllers();
+            services.AddCors();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseCors(m => m.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
